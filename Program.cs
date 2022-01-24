eed3f5906f55cd31fdc0155d4f078177f916113b
Will <twitchifygamers@gmail.com>
2022-01-24 10:26:24 +0000

    Added logic overview to the top

--- Program.cs
+++ Program.cs
@@ -10,7 +10,32 @@ namespace BubbleSortAlgorithm
 {
     class Program
     {
-        
+
+        /*  
+            INPUT the initial list of values //Array 
+
+            STORE the initial list of values in initiallist //Array 
+
+            OUTPUT "your initial list is " followed by initiallist //Array 
+
+            START LOOP COUNTER CONTROLLED UNTIL the length of Initiallist – 2 is met //For 
+
+            START LOOP COUNT CONTROLLED Until length of Initiallist – 2 //For 
+
+            IF intial list value == initiallist + 1 value //if 
+
+            STORE the value of initiallist + 1 in Tempvalue //Array 
+
+            SET initiallist + 1 = initiallist //Array 
+
+            SET initiallist = Tempvalue //Array 
+
+            End Loop //endfor 
+
+            End Loop //endfor 
+
+            Output - "Your sorted list is" followed by initial list  
+        */
 
         static void Main(string[] args)
         {

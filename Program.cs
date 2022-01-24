448320faebddc183dd33f83676338dc4963b5500
Will <twitchifygamers@gmail.com>
2022-01-24 10:35:16 +0000

    Removed all the waffle

--- Program.cs
+++ Program.cs
@@ -39,47 +39,7 @@ namespace BubbleSortAlgorithm
 
         static void Main(string[] args)
         {
-            try
-            {
-                int[] sequence = ConstructSequence(); // Get the sequence of numbers
-                Console.WriteLine(sequence.ToString()); // Output the sequence of numbers onto console
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine("'Main' method errored trying to retrieve sequence: " + e);
-            }
             
-        }
-
-        static int[] ConstructSequence() // This will create the sequence but initially will have a set array
-        {
-            try
-            {
-                int[] sequence = { 5, 10, 15, 20, 3, 2, 31, 45, 69, 100, 99 }; // The sequence of numbers used without the program
-                return sequence;
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine("Could not construct the sequence: " + e);
-            }
-        }
-
-        static void ShowUnsorted() // This will output the unsorted sequence
-        {
-            int[] sequence = ConstructSequence(); // Get the sequence and set it to an variable
-
-            string hello; // Set string to return at some point
-
-            foreach(int i in sequence) // Iterate over the numbers and add it to the string
-            {
-                hello = i + ", ";
-            }
-            return;
-        }
-
-        static void ShowSorted() // This will output the sorted sequence
-        {
-
         }
     }
 }

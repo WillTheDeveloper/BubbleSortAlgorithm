45fc61330bf8e4dfbfbc5c82a7063e52a72c6642
Will <twitchifygamers@gmail.com>
2022-01-24 10:05:43 +0000

    Added comments on most of the lines

--- Program.cs
+++ Program.cs
@@ -14,32 +14,32 @@ namespace BubbleSortAlgorithm
 
         static void Main(string[] args)
         {
-            int[] sequence = ConstructSequence();
-            Console.WriteLine(sequence);
+            int[] sequence = ConstructSequence(); // Get the sequence of numbers
+            Console.WriteLine(sequence); // Output the sequence of numbers onto console
             ShowUnsorted();
         }
 
-        static int[] ConstructSequence()
+        static int[] ConstructSequence() // This will create the sequence but initially will have a set array
         {
-            int[] sequence = { 5, 10, 15, 20, 3, 2, 31, 45, 69, 100, 99 };
+            int[] sequence = { 5, 10, 15, 20, 3, 2, 31, 45, 69, 100, 99 }; // The sequence of numbers used without the program
 
             return sequence;
         }
 
-        static void ShowUnsorted()
+        static void ShowUnsorted() // This will output the unsorted sequence
         {
-            int[] sequence = ConstructSequence();
+            int[] sequence = ConstructSequence(); // Get the sequence and set it to an variable
 
-            string hello;
+            string hello; // Set string to return at some point
 
-            foreach(int i in sequence)
+            foreach(int i in sequence) // Iterate over the numbers and add it to the string
             {
                 hello = i + ", ";
             }
             return;
         }
 
-        static void ShowSorted()
+        static void ShowSorted() // This will output the sorted sequence
         {
 
         }
